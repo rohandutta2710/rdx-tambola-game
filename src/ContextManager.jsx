b22fc0934259d5513777ec4c5b96cfaa8bf7a0a5
Rohan Dutta <rohandutta27102000@gmail.com>
2022-08-20 16:58:51 +0530

    Version 2

--- src/ContextManager.jsx
+++ src/ContextManager.jsx
@@ -1,7 +1,8 @@
 import { createContext, useState } from "react";
 const projectContext = createContext();
-const buttonClickSound = new Audio("./Images/new_pop_up1.mp3");
+
 function ContextManager(props) {
+    const buttonClickSound = new Audio("./Images/new_pop_up1.mp3");
     const [ticketDetails, setTicketDetails] = useState([]);
     const [numberDetails, setNumberDetails] = useState([]);
     const TicketClick = () => {
